8af5a2a47049b59e53d93967201d4e9b2b8b6587
Fred Carella <fred.carella@gmail.com>
2025-10-23 11:35:02 -0400

    commit

--- src/main/java/csd214_fall2025/entites/BookEntity.java
+++ src/main/java/csd214_fall2025/entites/BookEntity.java
@@ -118,11 +118,27 @@ public class BookEntity extends PublicationEntity {
 
     @Override
     public void edit() {
-
+        System.out.println("Edit Title (" + getTitle() + " [enter for no changes])");
+        setTitle(getInput(getTitle()));
+        System.out.println("Edit Author (" + getAuthor() + " [enter for no changes])");
+        setAuthor(getInput(getAuthor()));
+        System.out.println("Edit copies (" + getCopies() + " [enter for no changes])");
+        setCopies(getInput(getCopies()));
+        System.out.println("Edit price (" + getPrice() + " [enter for no changes])");
+        setPrice(getInput(getPrice()));
     }
 
     @Override
     public void initialize() {
+        System.out.println("Enter Title:");
+        setTitle(getInput("no title"));
+        System.out.println("Enter Author:");
+        setAuthor(getInput("no author"));
+        System.out.println("Enter copies:");
+        setCopies(getInput(0));
+        System.out.println("Enter price:");
+        setPrice(getInput(0.0d));
 
     }
+
 }
